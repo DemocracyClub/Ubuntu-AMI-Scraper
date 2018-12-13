2c3fb30c1aefb7283d792bb06198eab7ff3006f2
chris48s <chris.shaw480@gmail.com>
2018-12-13 09:10:56 +0000

    disable PRs for the next run

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ import scraperwiki
 
 
 SEND_NOTIFICATIONS = True
-OPEN_PULL_REQUESTS = True
+OPEN_PULL_REQUESTS = False
 
 try:
     SLACK_WEBHOOK_URL = os.environ['MORPH_UBUNTU_BOT_SLACK_WEBHOOK_URL']
